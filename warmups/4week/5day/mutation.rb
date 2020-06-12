0ca65faf1f98917cdb716b46fcf22383d1f59341
aleksanderbrymora <aleksander.australia@gmail.com>
2020-06-12 10:20:59 +1000

    solutions d5

--- warmups/4week/5day/mutation.rb
+++ warmups/4week/5day/mutation.rb
@@ -1,23 +1,23 @@
 # def mutation correct, bad
 # 	nice_print = ''
-# 	mutations = 0
+# 	mistakes = 0
 # 	correct.split('').map.with_index do |x, i|
-# 		if x === bad[i]
+# 		if x == bad[i]
 # 			nice_print += ' '
 # 		else
-# 			mutations += 1
 # 			nice_print += '^'
+# 			mistakes += 1
 # 		end
 # 	end
-# 	puts "#{mutations} mutations"
+# 	puts "#{mistakes} mutations in total"
 # 	puts correct, bad, nice_print
 # end
 
 def mutation correct, bad
-	differences = correct.split('').map.with_index { |x, i| x != bad[i]}
-	puts "#{differences.count true} mutations"
+	differences = correct.split('').map.with_index { |x, i| x != bad[i] }
+	puts "#{differences.count true} mutations in total"
 	puts correct, bad
-	puts differences.map {|x| x ? '^' : ' '}.join
+	puts differences.map { |x| x ? '^' : ' ' }.join
 end
 
 mutation 'GAGCCTACTAACGGGAT', 'CATCGTAATGACGGCCT'
