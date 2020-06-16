00c4c87fd504bb9e9d32fa86668d0b59c219a75a
aleksanderbrymora <aleksander.australia@gmail.com>
2020-06-17 08:56:35 +1000

    w5d3

---
+++ warmups/5week/3day/main.rb
@@ -0,0 +1,19 @@
+memory = []
+guessed = true
+
+while guessed
+	memory.push rand 1..4
+	puts memory.join ' '
+	sleep 1
+	puts `clear`
+	memory.map do |x|
+		if x == gets.to_i
+			puts `clear`
+		else
+			guessed = false
+			break
+		end
+	end
+end
+
+puts memory.size > 5 ? "Hot digidi damn you got #{memory.size}" : "Huh, lame. You got #{memory.size}"
