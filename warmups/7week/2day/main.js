8ccad87b71fd2d5aa4e9b7e64a006e1115337499
aleksanderbrymora <aleksander.australia@gmail.com>
2020-06-30 10:11:34 +1000

    w7d2s

---
+++ warmups/7week/2day/main.js
@@ -0,0 +1,44 @@
+const reverse = function (arr) {
+	const out = [];
+	for (let i = arr.length - 1; i >= 0; i--) {
+		out.push(arr[i]);
+	}
+	return out;
+};
+
+const flatten = function (arr) {
+	let out = [];
+	arr.forEach(function (el) {
+		if (Array.isArray(el)) {
+			out = out.concat(el);
+		} else {
+			out.push(el);
+		}
+	});
+	return out;
+};
+
+const flattenBonus = function (arr, out = []) {
+	arr.forEach(function (el) {
+		if (Array.isArray(el)) {
+			flattenBonus(el, out);
+		} else {
+			out.push(el);
+		}
+	});
+	return out;
+};
+
+console.log(reverse([1, 2, 3, 4]));
+console.log(flatten(['Hello', ['World', 42]]));
+console.log(
+	flattenBonus([
+		'hello',
+		[
+			[true, false, true],
+			'world',
+			42,
+			[1, 2, ['a', 'b', ['A', [0, 1, 2], 'C']]],
+		],
+	]),
+);
